7a545fbd690940e6c0726662fb63dc1e8a5fa790
Shivam Arora <shivamarora1@yahoo.com>
2025-01-20 23:41:16 +0530

    added nifty sensex stock values

--- backend/src/stocks/stocks.controller.ts
+++ backend/src/stocks/stocks.controller.ts
@@ -1,6 +1,6 @@
 import { Controller, Get, HttpStatus, Param, Res } from '@nestjs/common';
 import { Response } from 'express';
-import { StocksService } from './stocks.service';
+import { StocksService, BS_NIFTY, BS_SENSEX } from './stocks.service';
 
 @Controller('stocks')
 export class StocksController {
@@ -22,14 +22,19 @@ export class StocksController {
   async getOptions(@Res() res: Response) {
     // ! Logic is to call api and fetch data from multiple apis one by one.
     // ! call api to get options data.
+
+    const currencyRes = await this.stockService.getCurrencyRates();
+    const niftyRes = await this.stockService.getIndianIndicesFeed(BS_NIFTY);
+    const sensexRes = await this.stockService.getIndianIndicesFeed(BS_SENSEX);
+
     const response: any = {
       'Gold (22K)': '1gm = ₹ 77,409.34',
       'Gold (24K)': '1gm = ₹ 83,000,34',
       Silver: '1Kg = ₹ 91,000.45',
       'Crude Oil': '₹ 4,500',
-      Sensex: '₹ 3,400',
-      Nifty: '₹ 2,455',
-      USDINR: '₹ 86.12',
+      Sensex: `₹ ${(sensexRes as any).data.data.pricecurrent.toLocaleString()}`,
+      Nifty: `₹ ${(niftyRes as any).data.data.pricecurrent.toLocaleString()}`,
+      USDINR: `₹ ${(currencyRes as any).data.data.data[1][0].toLocaleString()}`,
     };
     return res.status(HttpStatus.OK).json(response);
   }
--- backend/src/stocks/stocks.service.ts
+++ backend/src/stocks/stocks.service.ts
@@ -2,6 +2,9 @@ import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
 import { AxiosResponse } from 'axios';
 
+export const BS_SENSEX = 'in;SEN';
+export const BS_NIFTY = 'in;NSX';
+
 @Injectable()
 export class StocksService {
   constructor(private readonly httpService: HttpService) {}
@@ -10,6 +13,11 @@ export class StocksService {
     indicesName = encodeURIComponent(indicesName);
     return this.httpService.axiosRef.get(
       `https://priceapi.moneycontrol.com/pricefeed/notapplicable/inidicesindia/${indicesName}`,
+    );
+  }
+  getCurrencyRates(): Promise<AxiosResponse<any>> {
+    return this.httpService.axiosRef.get(
+      `https://api.moneycontrol.com/mcapi/v1/currency/getGlobalCurrencyRates`,
     );
   }
 }
